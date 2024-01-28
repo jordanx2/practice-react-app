024c796fea29f181aaf100b42c669dfd8fc0db77
jordanx2 <jordanmooney9@gmail.com>
2024-01-28 20:46:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notify = (data) => toast('just clicked: ' + data);
+
+function Header(){
+  return (
+    <div className='App-Header'>
+      <ImageTextElement elementName='Search' imgLocation='/imgs/search.png'/>
+      <ImageTextElement elementName='Diary' imgLocation='/imgs/diary.png'/>
+      <ImageTextElement elementName='Back Office' imgLocation='/imgs/office.png'/>
+      <ImageTextElement elementName='Configuration' imgLocation='/imgs/config.png'/>
+      <ImageTextElement elementName='Till' imgLocation='/imgs/till.png'/>
+    </div>
+  );
+}
+
+function ImageTextElement({elementName, imgLocation}){
+  return (
+    <div className='image-text-container' onClick={() => notify(elementName)}>
+      <img src={imgLocation} alt={elementName + 'function'}></img>
+      <p> {elementName} </p>
+    </div>
+  );
+}
+
+function HomeItemsContainer({containerName, containerItems}){
+  return (
+    <div className='home-row-outer'>
+      <p className='home-row-container-name'>{containerName}</p>
+      <div className='home-row-inner'>
+        {containerItems.map(item => (
+          <ImageTextElement className='home-row-item' elementName={item} imgLocation='/imgs/testing.png'/>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function HomeRowContainer({containers}){
+  return (
+    <div className='home-row-container'>
+      { Object.entries(containers).map(([name, items]) => (
+        <HomeItemsContainer key={name} containerName={name} containerItems={items}/>
+      ))}
+      
+    </div>
+  );
+}
 
 function App() {
+  const row1 = {
+    'Patent Management' : ['Book Appointment', 'Find Patent', 'Web Patent Approval'],
+    'Examinations': ['Clinical Visit', 'Images and Documents', 'Referrals'],
+    'Sales': ['New Sale', 'Sales Worklists', 'Outstanding Payments', 'Reports'],
+    'Orders': ['Orders List', 'Orders Pickup']
+  };
+
+  const row2 = {
+    'End of Day' : ['Close Shift', 'Till Balancing', 'Reports', 'Outstanding Payments', 'Business Performance'],
+    'Product Management': ['Product Search', 'Product Update', 'Price Update'],
+    'Campaigns': ['Recall Maintainence', 'Templates', 'Programs'],
+    'NHS Claims': ['Claims List']
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <HomeRowContainer containers={row1} />
+      <HomeRowContainer containers={row2} />
+
+      <ToastContainer />
     </div>
   );
 }
